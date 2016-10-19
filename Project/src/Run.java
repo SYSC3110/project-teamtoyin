c38a1c71e0ff7ef1d994ba3553c9d5557262d7db
linaelsadek <linaelsadek@cmail.carleton.ca>
2016-10-18 20:08:33 -0400

    Code refactor, and bug fixes

--- Project/src/Run.java
+++ Project/src/Run.java
@@ -3,33 +3,58 @@ import java.util.Scanner;
 import Algorithm.*;
 import Network.*;
 
-
+/**
+ * @author Lina El Sadek
+ * @date 10/18/2016
+ * 
+ * 
+ */
 public class Run {
 
+	private static Network network;
+	private static Algorithm algorithm;
+	private static Message msg;
 	public static void main(String[] args)
 	{
+		//Variable definition and initialization
 		Scanner reader = new Scanner(System.in);
-		int nodeNum = 0;
-		int choice = 0;
 		String nodeName = "";
 		String sourceNode = "";
 		String destinationNode = "";
 		String msgStr = "";
-		Boolean flag = false;
-		int edgeNum = 0;
-		int counter = 0;
 		String edgeInput = "";
 		String doneCommand = "done";
 		String[] edgeInputArray = new String[2];
-		Network network = new Network();
-		Message msg;
-		Algorithm algorithm;
-		System.out.println("Welcome to Team Toyin's Network Algorithm Program");
-		System.out.println("First, we will build your network...");
+		Boolean flag = false;
+		int edgeNum = 0;
+		int counter = 0;
+		int nodeNum = 0;
+		int choice = 0;
+		network = new Network();
+		
+		//Prints welcome message
+		printWelcome();
+		
+		//Prompts the user to enter the number of nodes in the network
+		//making sure it's a positive integer
 		System.out.println("Please enter the number of nodes you want in your network");
-		nodeNum = reader.nextInt();
+		do {
+		    while (!reader.hasNextInt()) {
+		        System.out.println("That's not a number!");
+		        reader.next();
+		    }
+		    nodeNum = reader.nextInt();
+		    if(nodeNum <=0)
+		    {
+		    	System.out.println("That's not a positive number!");
+		    }
+		} while (nodeNum <= 0);
+		
+		//There can only be n*n edges in a network assuming all nodes are connected to all edges
 		edgeNum = nodeNum * nodeNum;
 		System.out.println("You have entered " + nodeNum + " as the number of nodes in your network");
+		
+		//Prompts the user to enter names for the nodes 
 		for(int i=0; i<nodeNum; i++)
 		{
 			System.out.println("Please enter the name of node number " + (i+1)+ " ");
@@ -47,26 +72,27 @@ public class Run {
 		System.out.println("Now that we have created the nodes, please enter the edges you want between the nodes");
 		System.out.println("A sample input would be: Node1 Node2");
 		edgeInput = reader.nextLine();
+		
+		//Prompts the user to enter the edges between the nodes
 		while(counter<edgeNum)
 		{	
 			if(doneCommand.equals(edgeInput.toLowerCase()))
 			{
 				break;
 			}
-			edgeInputArray = edgeInput.split(" ");
-			while(edgeInputArray.length <2)
+			
+			while(!validateInputString(edgeInput))
 			{
-				System.out.println("Oops, you have either entered less arguments than required, or did not enter it correctly");
-				System.out.println("Please Try again");
 				edgeInput = reader.nextLine();
-				edgeInputArray = edgeInput.split(" ");
 			}
+			edgeInputArray = edgeInput.split(" ");
 			network.link(edgeInputArray[0].toUpperCase(), edgeInputArray[1].toUpperCase());
 			counter++;
 			System.out.println("If you're done, please type done, or continue adding edges");
 			edgeInput = reader.nextLine();
 		}
 		
+		//Prompts the user to create a message, with source node, and destination node
 		System.out.println("Now enter the your message parameters");
 		System.out.print("\tMessage:");
 		msgStr = reader.nextLine();
@@ -86,20 +112,49 @@ public class Run {
 		}
 		msg = new Message(msgStr, sourceNode.toUpperCase(), destinationNode.toUpperCase());
 		algorithm = new RandomAlgorithm(network);
-		System.out.println("Now enter the algorithm you want to be used. Your Options are:");
+		
+		//Prints the Algorithm menu for the user
+		printAlgorithmMenu();
+		choice = reader.nextInt();
+		while(choice != 1)
+		{
+			System.out.println("You have entered an invalid option.");
+			printAlgorithmMenu();
+			choice = reader.nextInt();
+		}
+		
+		//Initialize the algorithm
+		flag = initializeAlgorithm(choice);
+		
+		//Runs the algorithm on the network
+		if(flag){
+			algorithm.run(msg);
+			System.out.println("Number of Packets generated: "+algorithm.getPacketCount());
+			System.out.println("Number of hops taken: " + msg.getHopCount());
+		}
+		
+		
+		reader.close();
+	}
+	
+	private static void printAlgorithmMenu()
+	{
+		System.out.println("Enter the algorithm you want to be used. Your Options are:");
 		System.out.println("1) Random Algorithm");
 		//System.out.println("2) Flooding Algorithm");
 		//System.out.println("3) Shortest Path Algorithm");
 		//System.out.println("4) To be Decided");
-		choice = reader.nextInt();
-		while(choice != 1)
-		{
-			System.out.println("You have entered an invalid option. Your Options are:");
-			System.out.println("1) Random Algorithm");
-			choice = reader.nextInt();
-		}
-		
-		flag = false;
+	}
+	
+	private static void printWelcome()
+	{
+		System.out.println("Welcome to Team Toyin's Network Algorithm Program");
+		System.out.println("First, we will build your network...");
+	}
+	
+	private static boolean initializeAlgorithm(int choice)
+	{
+		boolean flag = false;
 		switch(choice)
 		{
 		case 1:
@@ -117,14 +172,34 @@ public class Run {
 		default:
 			System.out.println("Invalid choice, no such algorithm");
 		}
+		return flag;
+	}
+	
+	private static boolean validateInputString(String str)
+	{
+		String[] strArray = new String[2];
 		
-		if(flag){
-			algorithm.run(msg);
-			System.out.println("Number of Packets generated: "+algorithm.getPacketCount());
-			System.out.println("Number of hops taken: " + msg.getHopCount());
+		strArray = str.split(" ");
+		if(strArray.length<2)
+		{
+			System.out.println("Oops, you have either entered less arguments than required, or did not enter it correctly");
+			System.out.println("Please Try again");
+			return false;
+		}	
+		else if(!network.contains(strArray[0].toUpperCase()))
+		{
+			System.out.println("Your left most argument is not in the network");
+			System.out.println("Please Try again");
+			return false;
 		}
+		else if(!network.contains(strArray[1].toUpperCase()))
+		{
+			System.out.println("Your right most argument is not in the network");
+			System.out.println("Please Try again");
+			return false;
+		}
+		else
+			return true;
 		
-		
-		reader.close();
 	}
 }
