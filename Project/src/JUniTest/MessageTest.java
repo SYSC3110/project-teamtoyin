ed5d1d43fabef2405908b96d29e3d26e7a5a8ced
toyinodujebe <toyinodujebe@cmail.carleton.ca>
2016-10-20 13:54:21 -0400

    Updated MessageTest.java

--- Project/src/JUniTest/MessageTest.java
+++ Project/src/JUniTest/MessageTest.java
@@ -29,6 +29,9 @@ public class MessageTest {
 	public void tearDown() throws Exception {
 	}
 	
+	/**
+	 * Test method for {@link Network.Message#Message(java.lang.String,java.lang.String,java.lang.String)}.
+	 */
 	@Test
 	public void testMessage(){
 		assertNotNull(message1);
@@ -36,6 +39,9 @@ public class MessageTest {
 		assertNotNull(message3);
 	}
 	
+	/**
+	 * Test method for {@link Network.Message#getSource()}.
+	 */
 	@Test
 	public void testgetSource(){
 		assertEquals("A", message1.getSource());
@@ -43,14 +49,29 @@ public class MessageTest {
 		assertEquals(null, message3.getSource());
 	} 
 	
+	/**
+	 * Test method for {@link Network.Message#setSource(java.lang.String)}.
+	 */
+	@Test 
+	public void testsetSource(){
+		message1.setSource("Toyin");
+		assertEquals("Toyin", message1.getSource());
+	}
+	
+	/**
+	 * Test method for {@link Network.Message#getDestination()}.
+	 */
 	@Test
 	public void testgetDestination(){
 		assertEquals("E", message1.getDestination());
 		assertEquals(null, message3.getDestination());
 	}
 	
+	/**
+	 * Test method for {@link Network.Message#getContents()}.
+	 */
 	@Test
-	public void testgetContenets(){
+	public void testgetContents(){
 		assertEquals("Hey", message1.getContents());
 		assertNotEquals("HEYY", message1.getContents());
 		assertEquals(null, message3.getContents());
