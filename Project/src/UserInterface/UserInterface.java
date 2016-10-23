950483ee261e68fbf2d91cbcb75aa90ed7eaf5ce
linaelsadek <linaelsadek@cmail.carleton.ca>
2016-10-23 15:49:33 -0400

    Added components to top panel(i.e. number of nodes, rate, and algorithm)

--- Project/src/UserInterface/UserInterface.java
+++ Project/src/UserInterface/UserInterface.java
@@ -1,7 +1,9 @@
 package UserInterface;
 
-import javax.swing.*;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.*;
 
 /**
  * 
@@ -18,17 +20,56 @@ public class UserInterface extends JFrame {
 	private JMenuItem undo;
 	private JMenuItem save;
 	private JMenuItem help;
+	
+	private JPanel top;
+	private JPanel middle;
+	private JPanel bottom;
+	private JPanel algorithmPanel;
+	
+	private JLabel algorithmlbl;
+	private JRadioButton randomAlgo;
+	private JRadioButton floodingAlgo;
+	private ButtonGroup algorithmGroup;
+	
+	private JLabel lblRate;
+	private JPanel PnlRate;
+	
+	private JPanel PnlNode;
+	private JLabel lblNode;
+	
+	private JTextField nodeNum;
+	private JTextField rateNum;
 
 	public UserInterface()
 	{
 		super("Team Toyin's Build-A-Network");
+		setLayout(new GridLayout(3, 1));
+		CreatePanels();
+		createNodeNum();
+		ChooseAlgorithm();
+		createRate();
 		createJMenu();
-		setSize(500,500);
-		setVisible(true);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		setSize(500,500);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 	
+	private void CreatePanels()
+	{
+		top = new JPanel();
+		top.setLayout(new GridLayout(1, 3));
+		
+		middle = new JPanel();
+		middle.setLayout(new GridLayout(1, 2));
+		
+		bottom = new JPanel();
+		bottom.setLayout(new BorderLayout());
+				
+		add(top);
+		add(middle);
+		add(bottom);
+	}
 	private void createJMenu()
 	{
 
@@ -62,17 +103,68 @@ public class UserInterface extends JFrame {
 		menuBar.add(menu);
 		
 		//add Menu Bar to Frame
-		setJMenuBar(menuBar);
+		setJMenuBar(menuBar);	
+	}
+	
+	private void createNodeNum()
+	{
+		PnlNode = new JPanel();
+		PnlNode.setLayout(new GridLayout(2, 1));
+		
+		lblNode = new JLabel("Number of Nodes");
+		
+		nodeNum = new JTextField();
+		nodeNum.setEditable(true);
+		nodeNum.setText("Number of Nodes");
 
 		
+		PnlNode.add(lblNode);
+		PnlNode.add(nodeNum);
+		top.add(PnlNode);
+	}
+	
+	private void ChooseAlgorithm()
+	{
+		algorithmPanel = new JPanel();
+		algorithmPanel.setLayout(new GridLayout(3, 1));
+		
+		algorithmlbl = new JLabel ("Choose an Algorithm");
+		algorithmPanel.add(algorithmlbl);
+		
+		algorithmGroup = new ButtonGroup();
+		randomAlgo = new JRadioButton("Random Algorithm");
+		floodingAlgo = new JRadioButton("Flooding Algorithm");
+		
+		algorithmGroup.add(randomAlgo);
+		algorithmGroup.add(floodingAlgo);
+		
+		algorithmPanel.add(randomAlgo);
+		algorithmPanel.add(floodingAlgo);
+		
+		
+		top.add(algorithmPanel);
 		
 	}
 	
+	private void createRate()
+	{
+		PnlRate = new JPanel();
+		PnlRate.setLayout(new GridLayout(2, 1));
+		
+		lblRate = new JLabel("Rate");
+		
+		rateNum = new JTextField();
+		rateNum.setEditable(true);
+		
+		PnlRate.add(lblRate);
+		PnlRate.add(rateNum);
+		
+		top.add(PnlRate);
+	}
 	
 	public static void main(String[] args)
 	{
 		UserInterface UI = new UserInterface();
-
 	}
 
 }
