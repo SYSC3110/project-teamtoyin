92649e0f25e0ae0e2b9336d0f1c910897117e094
toyinodujebe <toyinodujebe@cmail.carleton.ca>
2016-11-19 16:04:32 -0500

    Updated step

--- Project/src/Algorithm/DepthFirst.java
+++ Project/src/Algorithm/DepthFirst.java
@@ -1,6 +1,8 @@
 package Algorithm;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Queue;
 
 import Network.Message;
@@ -8,13 +10,11 @@ import Network.Network;
 import Network.Node;
 
 public class DepthFirst extends Algorithm {
-	private HashMap<Node, HashMap<Node, Node>> routing_table; //Depth first routing table (Current node, destination node, next node)
-	
+	private ArrayList<Node> visited;
 	/**
 	 * Constructor to assign network to the algorithm.
 	 */
 	public DepthFirst(Network network) throws NullPointerException{
-		HashMap<Node, Node> routing_entry;	//Routing table entry 
 		
 		//If network is null throw exception
 		if (network == null)
@@ -24,39 +24,71 @@ public class DepthFirst extends Algorithm {
 
 		// Initialize packet_count
 		this.packet_count = 0;
-		
-		//Initialize routing table
-		routing_table = new HashMap<Node, HashMap<Node, Node>>();
-		
-		//For each node in network initialize their routing entry
-		for (Node n: network.getNodes()) {
-			
-			//Initialize new hashmap
-			routing_entry = new HashMap<Node, Node>();
-			
-			//Add hashmap to routing table
-			routing_table.put(n, routing_entry);
-			
-		}
-		
-		//Load the routing table
-		this.loadRoutingTable();
-		
-	}
-	private void loadRoutingTable() {
-		// TODO Auto-generated method stub
-		Queue<Node> q; 										//Queue of nodes to visit 
-		Node curr_n;										//Node we are currently at
-		Node prev_n;										//Node we visited a node from
-		HashMap<Node, Node> visited;						//Hashmap of nodes visited and the node we visited from
-		HashMap<Node, Node> routing_entry;					//Entry in our routing table (destination node, next node)
-		
+
 	}
 	
+
 	@Override
 	protected boolean step() {
 		// TODO Auto-generated method stub
-		return false;
+
+		int index = 0; 	// Index in arraylist of messages
+		Node new_n; 	// New node to move message to
+
+		//If no more messages travelling in network and the network is not receiving new messages
+		if (!network.messagesMoving() && !network.isOpen()) {
+			System.out.println("No messages moving and network closed for new messages.");
+			return false;
+		}
+
+		//Iterator for messages in network
+		Iterator<Message> i = network.getMessages().iterator();
+		
+		//For each message in network
+		while (i.hasNext()) {
+			
+			//Get message
+			Message m = i.next();
+
+			// If the message is already at its destination, skip
+			if (m.getNode() == m.getDestination()) {
+				
+				//Debug
+				System.out.println(m.getContents() + " is at the destination node " + m.getNode().getName());
+				
+				//Node is at destination so remove it
+				i.remove();
+								
+				//Continue to next message
+				continue;
+				
+			} else {
+				
+				//Debug
+				System.out.println(m.getContents() + " is at node " + m.getNode().getName() + " and is going to node " + m.getDestination().getName());
+			
+			}
+
+			// Get node to move message to
+			new_n = this.next(m);
+			
+			//Update the messages position in the network
+			network.getMessages().get(index).setNode(new_n);
+			
+			//Increment packets sent
+			this.countPacket();
+			
+			//Count message hops
+			network.getMessages().get(index).countHop();
+			
+			//Increment index counter for messages arraylist
+			index ++;
+			
+		}
+		
+		//Step successfully completed
+		return true;
+		
 	}
 
 	@Override
