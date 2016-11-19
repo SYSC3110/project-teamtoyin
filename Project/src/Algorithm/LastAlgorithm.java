c8d9cb642c1028e44c6b5de84d0495537d43dce8
toyinodujebe <toyinodujebe@cmail.carleton.ca>
2016-11-19 16:46:03 -0500

    Had to start over to make depth first without using routing tables

---
+++ Project/src/Algorithm/LastAlgorithm.java
@@ -0,0 +1,20 @@
+package Algorithm;
+
+import Network.Message;
+import Network.Node;
+
+public class LastAlgorithm extends Algorithm {
+
+	@Override
+	protected boolean step() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected Node next(Message m) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
